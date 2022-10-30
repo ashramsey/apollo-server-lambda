251e5b407cd6005f7d5ee8a31c580e4bcb0fa09a
Ash Ramsey <ashramsey@gmail.com>
2022-10-30 19:20:40 +1100

    Basic schema directive working with @graphql-tools/schema v9.0.0 and graphql-modules v1.4.4

--- graphql.ts
+++ graphql.ts
@@ -1,14 +1,19 @@
 // // graphql.js
 
-import { ApolloServer, gql } from 'apollo-server-lambda'
+import { ApolloServer } from 'apollo-server-lambda'
+import { defaultFieldResolver } from "graphql";
 import {
   ApolloServerPluginLandingPageLocalDefault
 } from 'apollo-server-core'
+import { gql, createModule, createApplication, Injectable } from 'graphql-modules'
+import { SchemaDirectiveVisitor } from "@graphql-tools/utils"
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  directive @upper on FIELD_DEFINITION
+
   type Hello {
-    value: String
+    value: String @upper
   }
 
   type Query {
@@ -31,9 +36,34 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
+const moduleOne = createModule({
+  id: 'foo',
   typeDefs,
   resolvers,
+})
+
+const app = createApplication({
+  modules: [moduleOne]
+})
+
+class UpperCaseDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const { resolve = defaultFieldResolver } = field;
+    field.resolve = async function (...args) {
+      const result = await resolve.apply(this, args);
+      if (typeof result === "string") {
+        return result.toUpperCase();
+      }
+      return result;
+    };
+  }
+}
+SchemaDirectiveVisitor.visitSchemaDirectives(app.schema, {
+  upper: UpperCaseDirective,
+})
+
+const server = new ApolloServer({
+  schema: app.createSchemaForApollo(),
   csrfPrevention: true,
   cache: 'bounded',
   plugins: [
